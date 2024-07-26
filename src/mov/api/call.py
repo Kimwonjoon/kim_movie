76222963ac2ef7b593999ef169339f0c6cfdaa74
Kimwonjoon <kimpass189@gmail.com>
2024-07-26 14:06:42 +0900

    ing

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -1,15 +1,15 @@
-#import requests
-#import json
-def req(dt="20120101"):
+import requests
+def gen_url(dt="20120101"):
     base_url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
     key = '2df268e129e6b819167d9a47c41bedd0'
-
     url = f'{base_url}?key={key}&targetDt={dt}'
 
-#    res = requests.get(url)
-#    text = res.text
-#
-#    d = json.load(text)
-    print(url)
-req()
-    
+    return url
+
+def req(dt="20240725"):
+    url = gen_url(dt)
+    res = requests.get(url)
+    data = res.json()
+    code = res.status_code
+    return code, data
+print(req())
